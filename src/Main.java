7315e3a6c034e270c0d1057868b9867b7ef5f203
zegom <josepqmg@gmail.com>
2023-11-03 21:54:24 +0000

    First commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String windowTitle = "Quirky campaign";
+        //inserir code para mudar este título
+        JFrame frame = new JFrame();
+        frame.setSize(1000,700);
+        frame.setTitle(windowTitle);
+
+        frame.setVisible(true);
+
+    }
+}
